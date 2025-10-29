97d15ebbfa6b5d56351436e3bb4a90a3a3610c29
Antony Grace <graceantony202@gmail.com>
2025-10-29 16:41:19 +0000

    2025 not 2024 (_for element p_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,7 +109,7 @@ export default function LandingPage() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Emcoder. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2025 Emcoder. All rights reserved.</p>
       </footer>
     </div>
   );
